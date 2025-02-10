36270477b7524df145b65ca4eeb50b2c926c9c90
Junyoung Kim <codesep8@gmail.com>
2025-02-10 09:54:30 +0900

    Update auth.signin.tsx

--- app/routes/auth.signin.tsx
+++ app/routes/auth.signin.tsx
@@ -2,6 +2,10 @@ export default function SignInPage() {
     return (
         <div>
             signin
+            <form>
+                <input placeholder="minecraft name" />
+                <input placeholder="password" />
+            </form>
         </div>
     )
 }
