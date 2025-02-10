13701db66668f89bc37b58c8e585fc08f57032eb
dotnet <codesep8@gmail.com>
2025-02-10 19:43:58 +0900

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,8 +1,10 @@
 import { Header } from "@/components/layout/header";
+import { MetaData } from "@/components/metaData";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <div>
+            <MetaData />
             <Header/>
             <main>
                 {children}
