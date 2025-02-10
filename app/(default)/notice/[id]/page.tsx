2b3b29202bde24589616e007dd17a1b9cff301b9
dotnet <codesep8@gmail.com>
2025-02-10 18:32:41 +0900

    Update page.tsx

--- app/(default)/notice/[id]/page.tsx
+++ app/(default)/notice/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { MetaData } from "@/components/metaData";
 
-export default function NoticePage() {
+export default async function NoticePage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+    const id = (await params).id
     return (
         <div>
-            <MetaData title="공지"/>
+            <MetaData title={id}/>
             공지상세
         </div>
     )
