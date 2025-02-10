d5994679b1dca32d59a5549f995d7c9ae5192bde
dotnet <codesep8@gmail.com>
2025-02-10 19:24:15 +0900

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,14 +1,6 @@
 import { Header } from "@/components/layout/header";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-    title: {
-        template: `%s | Iodine`,
-        default: `Iodine`,
-    },
-    description: "Iodine의 공식 웹페이지입니다.",
-};
-
 export default function Layout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <div>
