d4655ef87710b35a67ee579f95ac05c2483e7f98
dotnet <codesep8@gmail.com>
2025-02-10 19:36:31 +0900

    Update metaData.tsx

--- components/metaData.tsx
+++ components/metaData.tsx
@@ -1,7 +1,16 @@
-export function MetaData({ title }: { title: string; }) {
+interface MetaTagProps {
+  title?: string;
+  description?: string;
+}
+
+export function MetaData(props: MetaTagProps) {
   return (
     <>
-        <title>{`${title} - 테스트`}</title>
+        <title>{(props.title && `${props.title} | Iodine`) || "Iodine"}</title>
+        <meta property="og:title" content={(props.title && `${props.title} | Iodine`) || "Iodine"} />
+        <meta property="og:site_name" content="Iodine" />
+        <meta name="description" content={props.description || "Iodine의 공식 웹페이지입니다."}/>
+        <meta property="og:description" content={props.description || "Iodine의 공식 웹페이지입니다."} />
     </>
   );
 }
