63dc98a450f647e26ab61d84adb5c0aeb9a29d05
codesep8 <codesep8@gmail.com>
2025-02-11 09:20:27 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { IBM_Plex_Sans_KR } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme";
 import { Announcement } from "@/components/layout/announcement";
-import { PrelineScript } from "@/components/providers/preline";
 import { Header } from "@/components/layout/header";
 import "./globals.css";
 import { ProgressBarProvider } from "@/components/providers/progress";
