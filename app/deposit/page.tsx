6359efb7f16ffcc6c362265211c5d6aa4e9f0aba
codesep8 <codesep8@gmail.com>
2025-02-11 09:15:47 +0900

    update

--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -1,6 +1,9 @@
+import { MetaData } from "@/components/metadata";
+
 export default function DepositPage() {
     return (
         <div>
+            <MetaData title="이체" />
             deposit
         </div>
     )
