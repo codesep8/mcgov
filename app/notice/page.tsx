1a5aec01f8691ba9e030b08d70e56944e28266b4
Junyoung Kim <codesep8@gmail.com>
2025-02-17 05:39:31 +0000

    d

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,7 +1,30 @@
-export default function NoticeListPage() {
+import { prisma } from "@/lib/prisma";
+import { unstable_cache } from "next/cache";
+
+const getNoticeList = unstable_cache(
+    async (page: number) => {
+        return await prisma.notices.findMany({
+            skip: (page - 1) * 10,
+            take: 10,
+            orderBy: { created_at: "desc" }
+        });
+    },
+    [],
+    { revalidate: 60 }
+);
+
+export default async function NoticeListPage({ searchParams }: { searchParams: { page?: string } }) {
+    const page = searchParams.page ? parseInt(searchParams.page, 10) : 1;
+    const notices = await getNoticeList(page);
+
     return (
         <div>
-            공지
+            <h1>공지 리스트</h1>
+            <ul>
+                {notices.map((notice) => (
+                    <li key={notice.id}>{notice.title}</li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
